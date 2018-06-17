cdcdef3558bc568af2b6cfebaf83bdc5f99504ae
Ryan <ryanwade1928@gmail.com>
2018-06-17 17:44:47 -0500

    Added addMoves() function and checkForMatch() function.

--- js/refactor.js
+++ js/refactor.js
@@ -7,33 +7,36 @@ $(document).ready(function() {
       $('.card').addClass('open show')
     }*/
   getList();
-  shuffle(listCards);
+
+  //shuffle(listCards);
   getDomCards(listCards);
-})
+});
 
 // * Create a list that holds all of your cards
 let listCards = [];
-
+let openedCards = [];
+let moveCounter = 0;
 
 //Event Listener for 'card' click
 $('.card').on('click', function() {
-  openCard(this);
-})
-
+  addMoves();
+  showCard(this);
+});
 
 function getList() {
   let cards = document.getElementsByClassName('card');
   for (let i = 0; i < cards.length; i++) {
     listCards.push(cards[i]);
   }
+
   return listCards;
 };
-
 
 //* shuffle the list of cards using the provided "shuffle" method below
 function shuffle(array) {
   var currentIndex = array.length,
     temporaryValue, randomIndex;
+  moveCounter = 0;
 
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
@@ -54,11 +57,33 @@ function getDomCards(array) {
   }
 }
 
-// *  - display the card's symbol (put this functionality in another function that you call from this one)*/
-function openCard(card) {
-  card.classList.add('match');
+// *  - display the card's symbol
+function showCard(card) {
+  $(card).addClass('open show');
+  addOpen(openedCards, card);
 }
 
-function openCard(card) {
-  $(card).addClass('open show');
+/*-add the card to a *list* of "open" cards (put this functionality in another
+function that you call from this one)*/
+function addOpen(array, card) {
+  openedCards.push(card);
+  checkForMatch(openedCards, card);
 }
+
+//- if the list already has another card, check to see if the two cards match
+function checkForMatch(array, card) {
+  console.log(newCard);
+  console.log(openedCards);
+}
+
+function addMoves() {
+  moveCounter++;
+  $('.moves').text(moveCounter);
+}
+
+/*
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
